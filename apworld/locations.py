ba75f964592d15da9616973b2e29515dc974b82e
TurtwigTrainer <nwjohnson7691@gmail.com>
2025-08-26 15:48:11 -0700

    Import and use TotalLocations data

--- apworld/locations.py
+++ apworld/locations.py
@@ -1,9 +1,21 @@
 from dataclasses import dataclass, field
 from itertools import count
 from typing import Iterable, List, Optional, Tuple, get_args
+from .options import TotalLocations
+# from environments, import environment data
 
 from BaseClasses import Location, LocationProgressType, Region
 
 class TTGrindWorksLocation(Location):
-    game = "Toontown: The Grindworks"
-    
+    game : str = "Toontown: The Grindworks"
+
+
+# using classic items ItemPickups check from RoR2 
+def get_classic_item_pickups(n: int) -> Dict[str, int]:
+    """Get n ItemPickups, capped at the max value for TotalLocations"""
+    n = max(n, 0)
+    n = min(n, TotalLocations.range_end)
+    return {f"ItemPickup{i + 1}": ror2_locations_start_id + i for i in range(n)}
+
+item_pickups = get_classic_item_pickups(TotalLocations.range_end)
+location_table = item_pickups
